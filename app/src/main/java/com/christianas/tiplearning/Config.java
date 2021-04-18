ab07e0ef575ab98fd32801130c75695c5cabe2b9
Kashif Ali Bhatti <kashif.alib42@gmail.com>
2021-04-19 00:09:52 +0500

    Paypal sandbox api changed

--- app/src/main/java/com/christianas/tiplearning/Config.java
+++ app/src/main/java/com/christianas/tiplearning/Config.java
@@ -2,5 +2,5 @@ package com.christianas.tiplearning;
 
 public class Config {
 
-    public static final String PAYPAL_CLIENT_ID = "ATx4qxg-lBV_xK8Kd1sjKf-YpkSECZawQPJT20Hs0YMBg-G-5UIw7IE85xvnaxFKm_0I9nqklbWm4gs_";
+    public static final String PAYPAL_CLIENT_ID = "ATWJAC02IzIZxQcFotSl_G0QyFnotu4aBPggR0agfsUiCNyMlOfsB2vurAmIQbDwP0Q6cZEtrtNR5YNn";
 }
